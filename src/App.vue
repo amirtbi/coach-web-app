d3cd0050f71a71ff0d7613a0221e074e76a7bbe1
amirtbi <torabi46@gmail.com>
2021-12-24 13:51:17 +0330

    importing header ui locally

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
+<the-header></the-header>
+<main>
   <router-view></router-view>
+</main>
 </template>
 
 <script>
+import TheHeader from './components/Layouts/TheHeader.vue';
 export default {
+  components:{
+    TheHeader
+  },
   data() {
-    return {
-     
-    }
+    return {};
   },
-  methods:{
-    
-  },
-  computed:{
-    
-  }
-}
+  methods: {},
+  computed: {},
+};
 </script>
-<style scoped>
-.blue-color{
-  color:red;
-  font-weight: bold;
-  font-size: 1.5rem;
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+body,
+html {
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: "Roboto", sans-serif;
 }
 </style>
