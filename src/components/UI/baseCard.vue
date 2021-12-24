6ceb19d16da94dd647e95e66f1b67460cf6c6a5b
amirtbi <torabi46@gmail.com>
2021-12-24 19:53:41 +0330

    define baseCard ui component

---
+++ src/components/UI/baseCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card-container">
+        <slot></slot>
+    </div>
+</template>
+
+
+<style scoped>
+.card-container{
+    box-shadow: 0px 0px 0px #c1c1c1;
+    max-width: 40rem;
+    box-shadow: 0px 2px 8px #c1c1c1;
+    padding:1rem;
+    margin: 2rem auto;
+    border-radius: 12px;
+}
+</style>
