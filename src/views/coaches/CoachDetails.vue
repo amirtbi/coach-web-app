50102459da84fba05ddd65cf1ae031c5399c45c1
amirtbi <torabi46@gmail.com>
2021-12-25 20:48:06 +0330

    adding coachdetails scripts and styles

--- src/views/coaches/CoachDetails.vue
+++ src/views/coaches/CoachDetails.vue
@@ -1,5 +1,89 @@
 <template>
-    Details for Coach
-    <router-view></router-view>
-    <router-link to="/coaches/c1/contact">Contact</router-link>
+  <section>
+    <base-card>
+      <span class="coach-title">Coach Name:</span><h3>{{ fullName }}</h3>
+      <span class="coach-rate">Rate:</span><h4>${{ rate }}/hour</h4>
+    </base-card>
+  </section>
+
+  <section>
+    <base-card>
+      <header>
+        <h2>Intrested? Reach out now!</h2>
+        <base-button link :to="contactCoach">Contact</base-button>
+      </header>
+
+      <router-view></router-view>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <base-badge
+        v-for="area in areas"
+        :type="area"
+        :key="area"
+        :title="area"
+      ></base-badge>
+      <p class="margin-top">{{ description }}</p>
+    </base-card>
+  </section>
 </template>
+
+<script>
+export default {
+  props: ["id"],
+  data() {
+    return {
+      selectedCoach: null,
+    };
+  },
+  computed: {
+    contactCoach() {
+      return this.$route.path + "/contact";
+    },
+    fullName() {
+      return this.selectedCoach.firstName + "" + this.selectedCoach.lastName;
+    },
+    rate() {
+      return this.selectedCoach.hourlyRate;
+    },
+    description() {
+      return this.selectedCoach.description;
+    },
+    areas() {
+      return this.selectedCoach.areas;
+    },
+  },
+  methods: {
+    // fetch requested coach info
+    fetchCoachInfo(coaches) {
+      this.selectedCoach = coaches.find((coach) => {
+        return coach.id === this.id;
+      });
+    },
+  },
+  created() {
+    // Load all data about the requested coach
+    const coachesList = this.$store.getters["coach/coaches"];
+    this.fetchCoachInfo(coachesList);
+  },
+};
+</script>
+
+<style scoped>
+header {
+  margin-bottom: 2rem;
+}
+.margin-top{
+    margin-top: 1.5rem;
+}
+h3,h4{
+    font-size: 1.2rem;
+    padding: 0;
+}
+span.coach-title,
+span.coach-rate{
+    font-size:1rem;
+    font-family: inherit;
+}
+</style>
