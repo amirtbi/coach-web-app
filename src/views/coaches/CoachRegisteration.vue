974e58ef951322d15ebcdd263884b42d5ce9c839
amirtbi <torabi46@gmail.com>
2021-12-27 21:21:15 +0330

    adding some methods and form component

--- src/views/coaches/CoachRegisteration.vue
+++ src/views/coaches/CoachRegisteration.vue
@@ -1,3 +1,27 @@
 <template>
-    Register
+    <coach-form @register-coach="addCoach"></coach-form>
 </template>
+
+<script>
+import coachForm from '../../components/Coach/coachForm.vue';
+export default{
+    components:{
+        coachForm
+    },
+    data() {
+        return {
+            
+        }
+    },
+    methods:{
+        addCoach(data){
+            this.$store.dispatch('coach/register',data);
+            this.$router.push({
+                path:'/coaches',
+                name:'coaches-list',
+            });
+            
+        }
+    }
+}
+</script>
