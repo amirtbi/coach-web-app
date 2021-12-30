364a05945e6283cfaf0e0c1868015283694ed8c0
amirtbi <torabi46@gmail.com>
2021-12-30 12:57:37 +0330

    adding isCoach method in coachGetters.js

--- src/store/modules/coaches/coachesGetters.js
+++ src/store/modules/coaches/coachesGetters.js
@@ -5,5 +5,12 @@ export default{
 
     hasCoaches(state){
         return state.coaches && state.coaches.length > 0;
+    },
+    // Check if the logged user is whether coach or not
+    isCoach(_,getters,_2,rootGetters){
+        const coaches= getters.coaches;
+        const userId = rootGetters.userId;
+        // return true, if at least one coach meets the condition
+        return coaches.some(coach => coach.id === userId);
     }
 }
