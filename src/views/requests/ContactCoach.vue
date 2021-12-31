b4d406946a1e8835b75a87eafef74cccac5232d8
amirtbi <torabi46@gmail.com>
2021-12-31 21:07:01 +0330

    add validation and some styles for contact form

--- src/views/requests/ContactCoach.vue
+++ src/views/requests/ContactCoach.vue
@@ -1,3 +1,86 @@
 <template>
-    Contact A Coach
+  <form @submit.prevent="submitForm">
+    <div class="form-control">
+      <label for="email">Your E-Mail</label>
+      <input
+        type="email"
+        id="email"
+        placeholder="name@email.com"
+        v-model.trim="email"
+      />
+    </div>
+    <div class="form-control">
+      <label for="message">Your message</label>
+      <textarea rows="5" id="message" v-model.trim="message"></textarea>
+    </div>
+    <p class="text-warning" v-if="!formIsValid">
+      You must fix errors or fill the blanks inputs
+    </p>
+    <div class="actions">
+      <base-button>Send Your Message</base-button>
+    </div>
+  </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      message: "",
+      formIsValid: true,
+    };
+  },
+  methods: {
+    submitForm() {
+      this.formIsValid = true;
+      if (
+        this.email === "" ||
+        !this.email.includes("@") ||
+        this.message === ""
+      ) {
+        this.formIsValid = false;
+        return;
+      }
+
+      const submittedMessage = {
+        email: this.email,
+        message: this.message,
+        coachId: this.$route.params.id,
+      };
+
+      this.$store.dispatch("request/addMessage", submittedMessage);
+      this.$router.push({ path: "/request" });
+    },
+  },
+};
+</script>
+<style scoped>
+div.form-control {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  width: 100%;
+}
+div.actions {
+  margin-top: 1rem;
+}
+div.form-control input,
+textarea {
+  width: 100%;
+  border: 1px solid #adadad;
+  padding: 0.5rem 1rem;
+  border-radius: 12px;
+}
+label {
+  font-weight: bold;
+  font-family: inherit;
+  margin-bottom: 1rem;
+  margin-top: 1rem;
+}
+.text-warning {
+  color: orangered;
+  font-weight: bold;
+}
+</style>
