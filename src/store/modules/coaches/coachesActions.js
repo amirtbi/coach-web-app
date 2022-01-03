2df247ced4c3ab24015ed48c6f84a4804735b524
amirtbi <torabi46@gmail.com>
2022-01-03 20:38:10 +0330

    using axios for fetching and sending data

--- src/store/modules/coaches/coachesActions.js
+++ src/store/modules/coaches/coachesActions.js
@@ -1,20 +1,30 @@
+import axios from "axios";
+
 export default {
   async register(context, payLoad) {
     console.log("adding new coaches");
-    const userId = context.rootGetters.userId;
+    //  const userId = context.rootGetters.newUser;
+    const userId = 'c5';
+ 
 
-    // STORING dataform in firebase database,
+    // Storing dataform in firebase database,
     // using fetch api
-    const response = await fetch(
-      `https://coach-app-c1d0e-default-rtdb.firebaseio.com/coaches/${userId}.json`,
-      {
-        method: "PUT",
-        body: JSON.stringify(payLoad),
-      }
-    );
+    // const response = await fetch(
+    //   `https://coach-app-c1d0e-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+    //   {
+    //     method: "PUT",
+    //     body: JSON.stringify(payLoad),
+    //   }
+    // );
+    // sending using Axios
 
+    const response = await axios({
+        method:'PUT',
+        url:`https://coach-app-c1d0e-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+        data:payLoad
+    })
     //const responseData = await response.json();
-    if (!response.ok) {
+    if (!response.statusText === 'ok') {
       // error
     }
     const newCoach = {
@@ -26,22 +36,32 @@ export default {
 
   // Load coaches from firebase
   async loadCoaches(context) {
-
     // Getting data from firebase
-    const response = await fetch(
-      "https://coach-app-c1d0e-default-rtdb.firebaseio.com/coaches.json"
-    );
+    const baseUrl =
+      "https://coach-app-c1d0e-default-rtdb.firebaseio.com/coaches.json";
 
-    // Waiting for json object    
-    const responseData = await response.json();
-    //console.log(responseData);    
-    if (!response.ok) {
-      /// error handling
+    const response = await axios.get(baseUrl);
+    console.log("data", response);
+    // const response = await fetch(
+    //   "https://coach-app-c1d0e-default-rtdb.firebaseio.com/coaches.json"
+    // );
+
+    // Waiting for json object
+    // In case of using fetch Api
+    // const responseData = await response.json();
+    // Getting data using Axios
+    const responseData = await response.data;
+
+    // In case of using fetch Api, statusText changes into (response.ok)
+    // if(!response.ok){//}
+    if (!response.statusText === "ok") {
+      const error = new Error(error.message || "fetching went wrong!");
+      throw error;
     }
 
     const coaches = [];
     for (let key in responseData) {
-      // Adding the coach  
+      // Adding the coach
       const coach = {
         id: key,
         firstName: responseData[key].firstName,
--- src/store/modules/coaches/store.js
+++ src/store/modules/coaches/store.js
@@ -7,15 +7,15 @@ export default {
   state() {
     return {
       coaches: [
-        //{
-        //   id: "c1",
-        //   firstName: "Maximilian",
-        //   lastName: "Schwarzmüller",
-        //   areas: ["frontend", "backend", "career"],
-        //   description:
-        //     "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
-        //   hourlyRate: 30,
-        // },
+        {
+          id: "c1",
+          firstName: "Maximilian",
+          lastName: "Schwarzmüller",
+          areas: ["frontend", "backend", "career"],
+          description:
+            "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
+          hourlyRate: 30,
+        },
         // {
         //   id: "c2",
         //   firstName: "Julie",
